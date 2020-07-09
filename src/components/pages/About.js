18e5fad66d46838deeaad7efed839a25d5f4a358
autumn998 <atarashiii998@gmail.com>
2020-07-09 15:20:00 +0630

    Done

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is The Todo List App v1.0.0 from Traversy Channel.</p>
+    </React.Fragment>
+  );
+}
+
+export default About;
